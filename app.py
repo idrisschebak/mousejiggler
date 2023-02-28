1db3d8812e8438381b6b3d1dcdee60c0c9d9987a
idrisschebak <idrisschebak@me.com>
2023-02-28 14:51:38 +0100

    delete timer add exit

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ import pyautogui
 import time
 import random
 import math
-import keyboard
 
 pyautogui.FAILSAFE = False
 
-def jiggle(distance, interval, speedup_prob, scale, stop):
+def jiggle(distance, interval, speedup_prob, scale, duration):
     """
-    Move the mouse cursor in a random pattern with the specified distance and interval indefinitely,
+    Move the mouse cursor in a random pattern with the specified distance and interval for the specified duration,
     following a random Gaussian curve with mean 0 and standard deviation 1.
     """
     speedup_factor = 1.0
     distance_factor = 1.0
     duration_factor = 1.0
+    end_time = time.time() + duration
 
-    while not stop:
+    while time.time() < end_time:
         # Generate random values for x and y offsets following a Gaussian distribution
         x_offset = int(random.gauss(0, 1) * scale * distance_factor)
         y_offset = int(random.gauss(0, 1) * scale * distance_factor)
@@ -44,14 +44,9 @@ distance = st.slider("Distance of mouse movement (in pixels)", min_value=10, max
 interval = st.slider("Interval between mouse movements (in seconds)", min_value=0.1, max_value=1.0, value=0.5, step=0.1)
 speedup_prob = st.slider("Probability of increasing mouse movement speed", min_value=0.0, max_value=1.0, value=0.1, step=0.1)
 scale = st.slider("Scaling factor for Gaussian distribution", min_value=10, max_value=30, value=20, step=5)
+duration = st.slider("Duration of mouse jiggling (in seconds)", min_value=10, max_value=3600, value=60, step=10)
 
 # Start the jiggler
-st.write("Press the 'q' key to stop jiggling.")
-stop = False
 jiggler_button = st.button("Start Jiggling")
 if jiggler_button:
-    stop = False
-    while not stop:
-        jiggle(distance, interval, speedup_prob, scale, stop)
-        if keyboard.is_pressed("q"):
-            stop = True
+    jiggle(distance, interval, speedup_prob, scale, duration)
